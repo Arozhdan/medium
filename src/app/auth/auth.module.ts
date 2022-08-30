b933719e8121991383f700f351b6bdedfbf6bffd
arozhdan <halala30@gmail.com>
2022-08-30 16:06:42 +0200

    feature: selectors init

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { RegisterComponent } from 'src/app/auth/register/register.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
 
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+  ],
+  declarations: [RegisterComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../store/actions/register.action';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    console.log(this.form.valid);
+    this.store.dispatch(registerAction(this.form.value));
+  }
 }
