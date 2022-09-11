5455f5e9f110637211a2e09b744451746527db83
arozhdan <halala30@gmail.com>
2022-09-11 18:13:28 +0200

    user profile module

---
+++ src/app/user-profile/store/effects/get-user-profile.effect.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { ArticleService as SharedArticleService } from 'src/app/shared/services/article.service';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+import {
+  getArticleAction,
+  getArticleFailureAction,
+  getArticleSuccessAction,
+} from '../actions/get-article.action';
+
+@Injectable()
+export class GetArticleEffect {
+  constructor(
+    private actions$: Actions,
+    private articleService: SharedArticleService
+  ) {}
+
+  getArticle$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getArticleAction),
+      switchMap(({ slug }) => {
+        return this.articleService.getArticle(slug).pipe(
+          map((article: ArticleInterface) => {
+            return getArticleSuccessAction({ article });
+          }),
+          catchError(() => {
+            return of(getArticleFailureAction());
+          })
+        );
+      })
+    )
+  );
+}
